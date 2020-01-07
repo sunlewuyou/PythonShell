8c556d5f2b1ef534d07b1bb1a301c8d73c1f1d17
Sunlewuyou <georgeguo1627@gmail.com>
2020-01-07 21:23:36 +0800

    Update CentOS_Python3.9_Auto_Install.sh

--- CentOS_Python3.9_Auto_Install.sh
+++ CentOS_Python3.9_Auto_Install.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel &&
+yum groupinstall -y "Development tools"
+yum install -y sqlite-devel ncurses-devel ncurses-libs zlib-devel mysql-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel openssl-devel
 wget https://www.python.org/ftp/python/3.9.0/Python-3.9.0a2.tar.xz &&
-mkdir -p /usr/local/python3 &&
-tar -xvf Python-3.9.0a2.tar.xz &&
-cd Python-3.9.0a2 &&
-./configure --prefix=/usr/local/python3 &&
+mkdir -p /usr/local/python3 && tar -xvf Python-3.9.0a2.tar.xz
+cd Python-3.9.0a2 && ./configure --prefix=/usr/local/python3 &&
 make && make install &&
 ln -s /usr/local/python3/bin/python3 /usr/local/bin/python3 &&
 ln -s /usr/local/python3/bin/pip3 /usr/local/bin/pip3 &&
