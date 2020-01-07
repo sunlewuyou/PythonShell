8bb8913b5d442308be673c80fd566785096a6936
Sunlewuyou <georgeguo1627@gmail.com>
2020-01-08 00:03:18 +0800

    Update CentOS_Python3.9_Auto_Install.sh

--- CentOS_Python3.9_Auto_Install.sh
+++ CentOS_Python3.9_Auto_Install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 yum groupinstall -y "Development tools"
-yum install -y sqlite-devel ncurses-devel ncurses-libs zlib-devel mysql-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel openssl-devel
+yum install -y sqlite-devel ncurses-devel ncurses-libs zlib-devel mysql-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel openssl-devel libffi-devel
 wget https://www.python.org/ftp/python/3.9.0/Python-3.9.0a2.tar.xz &&
-mkdir -p /usr/local/python3 && tar -xvf Python-3.9.0a2.tar.xz
+mkdir -p /usr/local/python3 && tar -xvf Python-3.9.0a2.tar.xz &&
 cd Python-3.9.0a2 && ./configure --prefix=/usr/local/python3 &&
 make && make install &&
 ln -s /usr/local/python3/bin/python3 /usr/local/bin/python3 &&
