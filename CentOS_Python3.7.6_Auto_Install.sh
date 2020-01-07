878cb4ef687533db59f7c398c06a45690769afe1
Sunlewuyou <georgeguo1627@gmail.com>
2020-01-08 00:41:43 +0800

    Update and rename CentOS_Python3.9_Auto_Install.sh to CentOS_Python3.7.6_Auto_Install.sh

--- CentOS_Python3.7.6_Auto_Install.sh
+++ CentOS_Python3.7.6_Auto_Install.sh
@@ -2,9 +2,9 @@
 
 yum groupinstall -y "Development tools"
 yum install -y sqlite-devel ncurses-devel ncurses-libs zlib-devel mysql-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel openssl-devel libffi-devel
-wget https://www.python.org/ftp/python/3.9.0/Python-3.9.0a2.tar.xz &&
-mkdir -p /usr/local/python3 && tar -xvf Python-3.9.0a2.tar.xz &&
-cd Python-3.9.0a2 && ./configure --prefix=/usr/local/python3 &&
+wget https://www.python.org/ftp/python/3.7.6/Python-3.7.6.tar.xz &&
+mkdir -p /usr/local/python3 && tar -xvf Python-3.7.6.tar.xz
+cd Python-3.7.6 && ./configure --prefix=/usr/local/python3 &&
 make && make install &&
 ln -s /usr/local/python3/bin/python3 /usr/local/bin/python3 &&
 ln -s /usr/local/python3/bin/pip3 /usr/local/bin/pip3 &&
@@ -15,5 +15,5 @@ pip3 install --upgrade pip &&
 echo "export PATH=/usr/local/python3/bin:$PATH" >> /etc/profile
 source /etc/profile
 cd ..
-rm -rf Python-3.9.0a2 Python-3.9.0a2.tar.xz
+rm -rf Python-3.7.6 Python-3.7.6.tar.xz
 echo "python3 installed successfullly!"
